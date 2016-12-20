7d2631ef95f085fab76a299bdf54ac46d0ce4016
Jonas Nilsson <jonas.nilsson@esss.se>
2016-12-20 19:42:15 +0100

    Added more tests.

---
+++ BaseLogHandlerStandIn.hpp
@@ -0,0 +1,13 @@
+//
+//  BaseLogHandlerStandIn.hpp
+//  dm-graylog-logger
+//
+//  Created by Jonas Nilsson on 2016-12-20.
+//  Copyright © 2016 European Spallation Source. All rights reserved.
+//
+
+#ifndef BaseLogHandlerStandIn_h
+#define BaseLogHandlerStandIn_h
+
+
+#endif /* BaseLogHandlerStandIn_h */
