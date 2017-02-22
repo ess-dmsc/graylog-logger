cefeaf33362c227e199ac5cc52699023cff1bf86
Jonas Nilsson <jonas.nilsson@esss.se>
2017-02-22 10:19:00 +0100

    Line break fix.

--- console_logger/ConsoleLogger.cpp
+++ console_logger/ConsoleLogger.cpp
@@ -170,7 +170,7 @@ void PrintAlternatives() {
     std::cout << "This application will write the log message to file and socket by default.\n";
     std::cout << "To prevent the application from doing this, use the -f and -a flags but do not\n";
     std::cout << "provide a file name or address. The level paramater is a value between 0  and 7\n";
-    std::cout << "with 0 being \"Emergency\" and 7 indicating a debug message. The default level";
+    std::cout << "with 0 being \"Emergency\" and 7 indicating a debug message. The default level\n";
     std::cout << "is 7 (debug). The default file name is \"messages.log\". The default address\n";
     std::cout << "is \"localhost\" and the default port is 12201.\n\n";
     std::cout << "Example: ./console_logger -t2.0 -m\"This is a log message.\"" << std::endl;
