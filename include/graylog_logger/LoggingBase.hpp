d37733d41b8c1c06429a7321ca89fb849931d9f5
Afonso Mukai <afonso.mukai@esss.se>
2017-05-17 15:01:13 +0200

    Apply clang-format LLVM style

--- include/graylog_logger/LoggingBase.hpp
+++ include/graylog_logger/LoggingBase.hpp
@@ -8,31 +8,35 @@
 
 #pragma once
 
+#include "graylog_logger/LogUtil.hpp"
+#include <mutex>
 #include <string>
 #include <vector>
-#include <mutex>
-#include "graylog_logger/LogUtil.hpp"
 
 class LoggingBase {
 public:
-    LoggingBase();
-    virtual ~LoggingBase();
-    virtual void Log(const Severity sev, const std::string &message);
-    virtual void Log(const Severity sev, const std::string &message, const std::vector<std::pair<std::string, AdditionalField>> &extraFields);
-    virtual void Log(const Severity sev, const std::string &message, const std::pair<std::string, AdditionalField> &extraField);
-    virtual void AddLogHandler(const LogHandler_P handler);
-    template<typename valueType>
-    void AddField(std::string key, const valueType &value) {
-        std::lock_guard<std::mutex> guard(baseMsgMutex);
-        baseMsg.AddField(key, value);
-    };
-    virtual void RemoveAllHandlers();
-    virtual void SetMinSeverity(Severity sev);
-    virtual std::vector<LogHandler_P> GetHandlers();
+  LoggingBase();
+  virtual ~LoggingBase();
+  virtual void Log(const Severity sev, const std::string &message);
+  virtual void
+  Log(const Severity sev, const std::string &message,
+      const std::vector<std::pair<std::string, AdditionalField>> &extraFields);
+  virtual void Log(const Severity sev, const std::string &message,
+                   const std::pair<std::string, AdditionalField> &extraField);
+  virtual void AddLogHandler(const LogHandler_P handler);
+  template <typename valueType>
+  void AddField(std::string key, const valueType &value) {
+    std::lock_guard<std::mutex> guard(baseMsgMutex);
+    baseMsg.AddField(key, value);
+  };
+  virtual void RemoveAllHandlers();
+  virtual void SetMinSeverity(Severity sev);
+  virtual std::vector<LogHandler_P> GetHandlers();
+
 protected:
-    Severity minSeverity;
-    std::mutex vectorMutex;
-    std::mutex baseMsgMutex;
-    std::vector<LogHandler_P> handlers;
-    LogMessage baseMsg;
+  Severity minSeverity;
+  std::mutex vectorMutex;
+  std::mutex baseMsgMutex;
+  std::vector<LogHandler_P> handlers;
+  LogMessage baseMsg;
 };
