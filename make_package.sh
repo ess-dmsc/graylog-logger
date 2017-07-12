f9922a8ccab4a66e59d98f95fcde51a20276a9cd
Afonso Mukai <afonso.mukai@esss.se>
2017-07-12 13:58:05 +0200

    Update script help string and error messages

--- make_package.sh
+++ make_package.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
+usage_str="\
+USAGE: make_package.sh [OPTIONS] <dir>
+  Substitute variables in conanfile and build package
+"
+
+description_str="\
+DESCRIPTION:
+  <dir> is a directory containing a conanfile.py and a test package. Its
+  contents are copied into a destination folder that must not already exist,
+  named conan_packaging by default, and version and commit values are
+  substituted. If the package is successfully generated, the destination
+  folder is removed afterwards. Version and commit can be defined by setting
+  the environment variables CONAN_PACKAGE_VERSION and CONAN_PACKAGE_COMMIT,
+  respectively, and the package channel can be set by defining
+  CONAN_CHANNEL.
+"
+
+options_and_returns_str="\
+OPTIONS:
+  -h  Print help and exit
+  -d  Destination folder name
+  -k  Keep destination package folder
+
+RETURNS:
+  0  success
+  1  packaging error
+  2  script error
+"
+
 usage() {
-    echo "USAGE: make_package.sh [OPTIONS] <dir>"
-    echo "  <dir> is a directory containing conanfile.py and test_package"
-    echo ""
-    echo "OPTIONS:"
-    echo "  -h          Print this message and exit"
-    echo "  -d          Destination folder"
-    echo "  -k          Keep destination package folder"
-    echo ""
-    echo "RETURNS:"
-    echo "  0  success"
-    echo "  1  packaging error"
-    echo "  2  script error"
+    printf "%s\n%s" \
+        "$usage_str" \
+        "$options_and_returns_str"
+}
+
+full_usage() {
+    printf "%s\n%s\n%s" \
+        "$usage_str" \
+        "$description_str" \
+        "$options_and_returns_str"
 }
 
 # Argument and option handling
@@ -27,9 +54,7 @@ while getopts "d:kh" arg; do
             keep_folder="TRUE"
             ;;
         h)
-            echo "$0: substitute variables in conanfile and build package"
-            echo ""
-            usage
+            full_usage
             exit 0
             ;;
     esac
@@ -47,6 +72,7 @@ fi
 if [ $# -gt 1 ] ; then
     >&2 echo "Error: too many arguments"
     >&2 echo ""
+    usage
     exit 2
 fi
 
@@ -55,6 +81,8 @@ conan_directory=$1
 if [ ! -d "$conan_directory" ] ; then
     >&2 echo "Error: invalid directory"
     >&2 echo "  $conan_directory"
+    >&2 echo ""
+    usage
     exit 2
 fi
 
@@ -65,6 +93,8 @@ fi
 if [ -d "$destination_folder" ] ; then
     >&2 echo "Error: destination folder already exists"
     >&2 echo "  $destination_folder"
+    >&2 echo ""
+    usage
     exit 2
 fi
 
@@ -107,7 +137,7 @@ current_dir="$(pwd)"
 cd "$destination_folder" && conan test_package
 result=$?
 if [ $result -ne 0 ] ; then
-    echo "Error: packaging failed with return code $result"
+    >&2 echo "Error: packaging failed with return code $result"
     exit 1
 fi
 cd "$current_dir"
