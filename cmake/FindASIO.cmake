9e5d71ed8cc5f0f45c5b38b76410598e6c2724f2
Matthew D Jones <matthew.d.jones@tessella.com>
2019-05-29 09:44:25 +0100

    Define target for asio and use the one now provided by the json library

--- cmake/FindASIO.cmake
+++ cmake/FindASIO.cmake
@@ -18,3 +18,14 @@ mark_as_advanced(
   ASIO_ROOT_DIR
   ASIO_INCLUDE_DIR
 )
+
+if(ASIO_FOUND)
+    set(ASIO_INCLUDE_DIRS ${ASIO_INCLUDE_DIR})
+endif()
+
+if(ASIO_FOUND AND NOT TARGET ASIO::ASIO)
+    add_library(ASIO::ASIO INTERFACE IMPORTED)
+    set_target_properties(ASIO::ASIO PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${ASIO_INCLUDE_DIR}"
+            )
+endif()
