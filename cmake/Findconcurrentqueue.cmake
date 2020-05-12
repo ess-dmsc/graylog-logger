59d3e6fd11a409d8ca6decbe3536fd03b7f37e9a
Jonas Nilsson <mjc.nilsson@gmail.com>
2020-05-12 15:29:36 +0200

    CMake fixes.

--- cmake/Findconcurrentqueue.cmake
+++ cmake/Findconcurrentqueue.cmake
@@ -1,24 +1,24 @@
-find_path(ConcurrentQueue_INCLUDE_DIR
+find_path(concurrentqueue_INCLUDE_DIR
   NAMES concurrentqueue/concurrentqueue.h
   PATHS /usr/local/include/ /opt/local/include/
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ConcurrentQueue FOUND_VAR ConcurrentQueue_FOUND REQUIRED_VARS
-  ConcurrentQueue_INCLUDE_DIR
+find_package_handle_standard_args(concurrentqueue FOUND_VAR concurrentqueue_FOUND REQUIRED_VARS
+  concurrentqueue_INCLUDE_DIR
 )
 
 mark_as_advanced(
-  ConcurrentQueue_INCLUDE_DIR
+  concurrentqueue_INCLUDE_DIR
 )
 
-if(ConcurrentQueue_FOUND)
-    set(ConcurrentQueue_INCLUDE_DIRS ${ConcurrentQueue_INCLUDE_DIR})
+if(concurrentqueue_FOUND)
+    set(concurrentqueue_INCLUDE_DIRS ${concurrentqueue_INCLUDE_DIR})
 endif()
 
-if(ConcurrentQueue_FOUND AND NOT TARGET concurrentqueue::concurrentqueue)
+if(concurrentqueue_FOUND AND NOT TARGET concurrentqueue::concurrentqueue)
     add_library(concurrentqueue::concurrentqueue INTERFACE IMPORTED)
     set_target_properties(concurrentqueue::concurrentqueue PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${ConcurrentQueue_INCLUDE_DIR}"
+            INTERFACE_INCLUDE_DIRECTORIES "${concurrentqueue_INCLUDE_DIR}"
             )
 endif()
